d438ba21a580919e7f5685d22782532eeab2b379
Andre Carr <129695312+Andre-Carr@users.noreply.github.com>
2023-10-27 20:06:59 -0400

    Create getComputerChoice function that returns a random choice of rock, paper, or scissors

--- rockPaperScissors.js
+++ rockPaperScissors.js
@@ -1,6 +1,8 @@
+"use strict";
 
 function getComputerChoice() {
-
+    let choices = new Array('rock', 'paper', 'scissors');
+    return choices[Math.floor(Math.random() * 2)];
 }
 
 function promptPlayerChoice(testValue) {
